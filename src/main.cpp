8308d6e2135c3611ebcabaab0b4c51a70b3a6fbb
Chris Rizzitello <sithlord48@gmail.com>
2016-10-23 19:39:38 -0400

    Add klin and use it
    Signed-off-by: Chris Rizzitello <sithlord48@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,41 @@
- 
-int main (void)
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QTimer>
+#include <QTextStream>
+
+#include "kiln.h"
+
+int main(int argc, char *argv[])
 {
-return 0;
+    QCoreApplication app(argc, argv);
+    app.setApplicationName(QString::fromLatin1("Kiln"));
+    app.setApplicationVersion(QString::fromLatin1("0.1"));
+
+    QTextStream stream(stdout);
+    Kiln *kiln = new Kiln;
+    QString device;
+    int baud=115200;
+    QString fileName;
+    if (argc >1) {
+        for (int i=1; i < argc; i++) {
+            if (QString::fromLatin1(argv[i]) == QString::fromLatin1("-d")) {
+                device = QString::fromLatin1(argv[i+1]);
+            } else if (QString::fromLatin1(argv[i]) == QString::fromLatin1("-b")) {
+                baud = QString::fromLatin1(argv[i+1]).toInt();
+            } else if (QString::fromLatin1(argv[i]) == QString::fromLatin1("-p")) {
+                fileName = QString::fromLatin1(argv[i+1]);
+            }
+        }
+    }
+    if (!device.isEmpty()) {
+        stream << "Connecting to:" <<device <<" baud:" << baud <<endl;
+        kiln->setDevice(device, baud);
+    } else {
+        stream << "A Device is Required"<< endl; return 0;
+    }
+
+    if(!fileName.isEmpty()) {
+        kiln->print(fileName);
+    }
+    return app.exec();
 }
